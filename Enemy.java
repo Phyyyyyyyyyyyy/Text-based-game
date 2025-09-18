74e912446f3fd670b7dcd274f67d6ba016d2b86f
Reuben <118543425+Phyyyyyyyyyyyy@users.noreply.github.com>
2025-09-19 07:49:01 +0800

    Update Enemy.java
    implementation of enemy cpu characters

--- Enemy.java
+++ Enemy.java
@@ -32,5 +32,17 @@ public class Enemy {
         System.out.println("Skill 2: " + skill2);
         System.out.println("Skill 3: " + skill3);
     }
+
+    public static Enemy getRandomEnemy() {
+        List<Enemy> enemies = new ArrayList<>();        
+        enemies.add(new Enemy("Loki", 100, 100, 15, "Illusion - avoids damage", "Scepter Strike - 20 damage", "Mind Control - 30 damage", 20, 20, 30, 100));
+        enemies.add(new Enemy("Ultron", 120, 120, 18, "Laser Blast - 25 damage", "Metal Punch - 15 damage", "Self-Repair - heals 20 HP", 25, 15, 20, 100));
+        enemies.add(new Enemy("Red Skull", 110, 110, 16, "Cosmic Blast - 30 damage", "Tactical Strike - 20 damage", "Rally Troops - doubles attack", 30, 20, 20, 100));
+        enemies.add(new Enemy("Venom", 130, 130, 17, "Symbiote Strike - 25 damage", "Web Trap - 15 damage", "Rage - doubles attack", 25, 15, 20, 100));         //arraylist of enemy characters for cpu/ai/bot
+
+        Random rand = new Random();       //to select random enem
+        return enemies.get(rand.nextInt(enemies.size()));           //return enemy object for main
+    }
     
 }
+
