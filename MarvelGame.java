857fe88c8eebb401884fb66c6aac08e0e21ef65d
Reuben <118543425+Phyyyyyyyyyyyy@users.noreply.github.com>
2025-10-17 22:45:07 +0800

    Update MarvelGame.java

--- MarvelGame.java
+++ MarvelGame.java
@@ -22,10 +22,7 @@ public class MarvelGame {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-     
         MainMenu.start(sc);
-
         sc.close();
     }
 }
