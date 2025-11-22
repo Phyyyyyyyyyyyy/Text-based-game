919fafce3ea080e38e4b8beed5f58d010caeb4d5
Reuben <118543425+Phyyyyyyyyyyyy@users.noreply.github.com>
2025-11-22 09:06:10 +0800

    Remove unused typeWriter method from MarvelGame

--- MarvelGame.java
+++ MarvelGame.java
@@ -5,25 +5,4 @@ public class MarvelGame {
         MainMenu.start(sc);
         sc.close();
     }
-
-
-
-
-
-
-
-
-
-
-    public static void typeWriter(String text, int delay) {
-        for (char c : text.toCharArray()) {
-            System.out.print(c);
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-        System.out.println();
-    }
 }
