30ecc8298895d83849dc42a418ba3c2a6929d2e4
Reuben <118543425+Phyyyyyyyyyyyy@users.noreply.github.com>
2025-11-27 00:40:13 +0800

    Update EnemyHierarchy.java

--- EnemyHierarchy.java
+++ EnemyHierarchy.java
@@ -20,67 +20,68 @@ public class EnemyHierarchy {
 
     private static void initializeEnemies() {
         // EASY ENEMIES (5) - Low HP and damage
-        easyEnemies.add(new Enemy("Loki", 80, 80, 12,
-                "Illusion Sneak Attack - 10 damage", "Scepter Strike - 15 damage", "Mind Control - 18 damage",
-                10, 15, 18, 10, 15, 18, 80));
+        easyEnemies.add(new Enemy("Loki", 70, 70, 12,
+                "Illusion Sneak Attack - 5 damage", "Scepter Strike - 10 damage", "Mind Control - 10 damage",
+                5, 10, 10, 5, 10, 10, 80));
 
-        easyEnemies.add(new Enemy("Mystique", 80, 80, 13,
+        easyEnemies.add(new Enemy("Mystique", 70, 70, 11,
                 "Shape Shift Strike - 10 damage", "Mimic Attack - 15 damage", "Invisible Hit - 18 damage",
                 10, 15, 18, 10, 15, 18, 80));
 
-        easyEnemies.add(new Enemy("Green Goblin", 85, 85, 12,
+        easyEnemies.add(new Enemy("Green Goblin", 75, 70, 12,
                 "Pumpkin Bomb - 10 damage", "Glider Attack - 15 damage", "Goblin's Rage - 18 damage",
                 10, 15, 18, 10, 15, 18, 85));
 
-        easyEnemies.add(new Enemy("Electro", 75, 75, 11,
+        easyEnemies.add(new Enemy("Electro", 70, 70, 11,
                 "Electric Shock - 10 damage", "Thunderbolt - 12 damage", "Overcharge - 15 damage",
                 10, 12, 15, 10, 12, 15, 75));
 
-        easyEnemies.add(new Enemy("Sandman", 90, 90, 11,
+        easyEnemies.add(new Enemy("Sandman", 70, 70, 11,
                 "Sand Blast - 10 damage", "Morphing Strike - 12 damage", "Sand Smash - 15 damage",
                 10, 12, 15, 10, 12, 15, 90));
 
         // MEDIUM ENEMIES (5) - Medium HP and damage
-        mediumEnemies.add(new Enemy("Ultron", 110, 110, 16,
-                "Laser Blast - 13 damage", "Metal Punch - 15 damage", "Flight Thrust Attack - 18 damage",
-                13, 15, 18, 13, 15, 18, 100));
+        mediumEnemies.add(new Enemy("Ultron", 100, 100, 15,
+                "Laser Blast - 13 damage", "Metal Punch - 13 damage", "Flight Thrust Attack - 15 damage",
+                13, 13, 15, 13, 13, 15, 100));
 
-        mediumEnemies.add(new Enemy("Red Skull", 105, 105, 15,
-                "Cosmic Blast - 13 damage", "Tactical Strike - 15 damage", "Rally Troops - 18 damage",
-                13, 15, 18, 13, 15, 18, 100));
+        mediumEnemies.add(new Enemy("Red Skull", 100, 100, 15,
+                "Cosmic Blast - 10 damage", "Tactical Strike - 10 damage", "Rally Troops - 15 damage",
+                10, 10, 15, 10, 10, 15, 100));
 
-        mediumEnemies.add(new Enemy("Venom", 115, 115, 16,
-                "Symbiote Strike - 13 damage", "Web Trap - 15 damage", "Rage - 18 damage",
-                13, 15, 18, 13, 15, 18, 100));
+        mediumEnemies.add(new Enemy("Venom", 100, 100, 15,
+                "Symbiote Strike - 10 damage", "Web Trap - 15 damage", "Rage - 18 damage",
+                10, 15, 18, 10, 15, 18, 100));
 
-        mediumEnemies.add(new Enemy("Doctor Octopus", 110, 110, 16,
-                "Tentacle Slam - 13 damage", "Mechanical Grab - 15 damage", "Overload - 18 damage",
-                13, 15, 18, 13, 15, 18, 100));
+        mediumEnemies.add(new Enemy("Doctor Octopus", 110, 110, 15,
+                "Tentacle Slam - 10 damage", "Mechanical Grab - 15 damage", "Overload - 18 damage",
+                10, 15, 18, 10, 15, 18, 100));
 
-        mediumEnemies.add(new Enemy("Sabretooth", 108, 108, 17,
+        mediumEnemies.add(new Enemy("Sabretooth", 104, 104, 15,
                 "Ferocious Claw - 13 damage", "Savage Bite - 15 damage", "Howling Vengeance - 18 damage",
-                13, 15, 18, 13, 15, 18, 100));
+                10, 13, 16, 10, 13, 16, 100));
 
         // HARD ENEMIES (5) - High HP and damage
-        hardEnemies.add(new Enemy("Magneto", 130, 130, 19,
-                "Magnetic Pulse - 16 damage", "Metal Manipulation - 18 damage", "Force Field Attack - 20 damage",
-                16, 18, 20, 16, 18, 20, 120));
+        hardEnemies.add(new Enemy("Magneto", 120, 120, 15,
+                "Magnetic Pulse - 15 damage", "Metal Manipulation - 15 damage", "Force Field Attack - 20 damage",
+                12, 14, 16, 12, 14, 16, 120));
 
-        hardEnemies.add(new Enemy("Hela", 135, 135, 20,
+        hardEnemies.add(new Enemy("Hela", 120, 120, 15,
                 "Necrosword Slash - 16 damage", "Minions Attack - 18 damage", "Asgardian Fury - 20 damage",
-                16, 18, 20, 16, 18, 20, 120));
+                12, 14, 16, 12, 14, 16, 120));
 
-        hardEnemies.add(new Enemy("Kingpin", 125, 125, 18,
-                "Heavy Punch - 16 damage", "Ground Slam - 18 damage", "Intimidate - 20 damage",
-                16, 18, 20, 16, 18, 20, 120));
+        hardEnemies.add(new Enemy("Kingpin", 120, 120, 15,
+                "Heavy Punch - 14 damage", "Ground Slam - 16 damage", "Intimidate - 18 damage",
+                12, 14, 16, 12, 14, 16, 120));
 
-        hardEnemies.add(new Enemy("Juggernaut", 140, 140, 21,
-                "Unstoppable Charge - 16 damage", "Ground Pound - 18 damage", "Rage Mode - 20 damage",
-                16, 18, 20, 16, 18, 20, 120));
+        hardEnemies.add(new Enemy("Juggernaut", 125, 125, 15,
+                "Unstoppable Charge - 14 damage", "Ground Pound - 16 damage", "Rage Mode - 18 damage",
+                14, 16, 18, 14, 16, 18, 120));
 
-        hardEnemies.add(new Enemy("Thanos", 150, 150, 22,
-                "Power Stone Punch - 18 damage", "Space Stone Snap - 20 damage", "Reality Warp - 22 damage",
-                18, 20, 22, 18, 20, 22, 130));
+        //////finals bows
+        hardEnemies.add(new Enemy("Thanos", 135, 135, 22,
+                "Power Stone Punch - 10 damage", "Space Stone Snap - 15 damage", "Reality Warp - 20 damage",
+                10, 15, 20, 10, 15, 20, 130));
     }
 
     public static Enemy getEnemyByDifficulty(Difficulty difficulty) {
