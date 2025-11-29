121b9351f99b48bc33c97a4662734740926dfc17
Reuben <118543425+Phyyyyyyyyyyyy@users.noreply.github.com>
2025-11-29 23:41:26 +0800

    Update Player1.java

--- Player1.java
+++ Player1.java
@@ -139,7 +139,8 @@ public class Player1 extends SelectScreen {
                         break;
                     case 9:
                         playSound("InputSound.wav");
-                        MainMenu.start(sc);
+                        MainMenu menu= new MainMenu(sc);
+                        menu.start(sc);
                         return null;
                     case 69:
                         playSound("InputSound.wav");
